996d0def76d78c7b4fb6cd959ba6470212bc58ee
kellipilvet <kelli.pilvet@gmail.com>
2025-11-30 21:20:41 +0200

    Update sample_queries.sql

--- sql/sample_queries.sql
+++ sql/sample_queries.sql
@@ -2,6 +2,9 @@
 SELECT company_name, registry_code
 FROM db_demo.dim_company;
 
+SELECT company_name, registry_code_masked
+FROM db_demo.dim_company_masked;
+
 --- Group companies by county
 SELECT
     splitByChar(',', normalized_address)[1] AS county,
@@ -10,7 +13,18 @@ FROM db_demo.dim_company
 GROUP BY county
 ORDER BY companies DESC;
 
+SELECT
+    splitByChar(',', normalized_address_masked)[1] AS county,
+    count(*) AS companies
+FROM db_demo.dim_company_masked
+GROUP BY county
+ORDER BY companies DESC;
+
 --- Find companies with 5-digit postal codes starting with '44'
 SELECT registry_code, company_name, postal_code
 FROM db_demo.dim_company
 WHERE postal_code LIKE '44%';
+
+SELECT registry_code_masked, company_name, postal_code_masked
+FROM db_demo.dim_company_masked
+WHERE postal_code_masked LIKE '44%';
