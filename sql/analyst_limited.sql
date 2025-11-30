004e8c5832a505da4cc9cf33d6e65e84e73ddfdc
kellipilvet <kelli.pilvet@gmail.com>
2025-11-30 16:42:04 +0200

    Update analyst_limited.sql

--- sql/analyst_limited.sql
+++ sql/analyst_limited.sql
@@ -10,5 +10,8 @@ GRANT analyst_limited TO limited_user;
 
 -- give the user select rights only on required columns.
 GRANT SELECT (company_id, company_name, vat_code, initial_registration_date, legal_form, legal_form_subtype, valid_from, valid_to)
-ON TABLE dim_company
+ON TABLE db_demo.dim_company
 TO analyst_limited;
+
+GRANT SELECT ON db_demo.dim_company_masked TO analyst_limited;
+
