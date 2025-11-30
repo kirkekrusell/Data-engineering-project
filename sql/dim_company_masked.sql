8541b5b9849f79fe1fe91fe9e05fdfaf1b1d6e2e
kellipilvet <kelli.pilvet@gmail.com>
2025-11-30 16:18:02 +0200

    Create dim_company_masked.sql

---
+++ sql/dim_company_masked.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE VIEW dim_company_masked AS
+SELECT
+
+    --- registry_code: keep only last 4 digits
+    concat('***', right(registry_code, 4)) AS registry_code_masked,
+
+    company_id,
+    company_name,
+    vat_code,
+    initial_registration_date,
+
+    --- normalized_adress: replace with NULL
+    NULL AS normalized_address_masked,
+      
+    --- postal_code: keep only first 2 digits
+    concat('***', left(postal_code, 2)) AS postal_code_masked,
+
+    legal_form,
+    legal_form_subtype,
+    valid_from,
+    valid_to
+
+FROM dim_company;
