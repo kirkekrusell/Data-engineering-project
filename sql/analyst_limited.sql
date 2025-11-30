f70f76c1e749df5dac10acc4d63a538d98203aaa
kellipilvet <kelli.pilvet@gmail.com>
2025-11-30 20:23:25 +0200

    Update analyst_limited.sql

--- sql/analyst_limited.sql
+++ sql/analyst_limited.sql
@@ -8,6 +8,9 @@ IDENTIFIED BY 'limited_strong_password';
 -- give the user this role 
 GRANT analyst_limited TO limited_user;
 
+--- give the user the usage rights to db_demo
+GRANT USAGE ON DATABASE db_demo TO analyst_limited;
+
 -- give the user select rights only on required columns.
 GRANT SELECT (company_id, company_name, vat_code, initial_registration_date, legal_form, legal_form_subtype, valid_from, valid_to)
 ON db_demo.dim_company
