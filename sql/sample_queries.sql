479f43a0937dd62ecf949620365db8c8cdceeba9
kellipilvet <kelli.pilvet@gmail.com>
2025-11-30 19:39:31 +0200

    Create sample_queries.sql

---
+++ sql/sample_queries.sql
@@ -0,0 +1,17 @@
+--- Find companies with VAT codes
+SELECT company_name, vat_code
+FROM db_demo.dim_company
+WHERE vat_code != '';
+
+--- Group companies by county
+SELECT
+    splitByChar(',', normalized_address)[1] AS county,
+    count(*) AS companies
+FROM db_demo.dim_company
+GROUP BY county
+ORDER BY companies DESC;
+
+--- Find companies with 5-digit postal codes starting with '44'
+SELECT registry_code, company_name, postal_code
+FROM db_demo.dim_company
+WHERE postal_code LIKE '44%';
